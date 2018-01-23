b912f08b0e7b32403afb18149c8ab09b3302b263
Aliaksei Kazhura <aliakseikazhura@gmail.com>
2018-01-23 03:12:30 +0300

    commit

--- src/Task_557.java
+++ src/Task_557.java
@@ -1,7 +1,6 @@
-/**
- * Created by Алексей on 23.01.2018.
- */
 public class Task_557 {
     public static void main(String[] args) {
+        System.out.println("557");
+        System.out.println();
     }
 }
